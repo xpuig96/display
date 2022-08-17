30f042e8d3ecae973b3b8c564fa8fe92785fc653
xavi <xpuiig14@gmail.com>
2022-08-17 21:06:03 +0200

    primer commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+
+  constructor(private fb: FormBuilder, private _snackBar: MatSnackBar) { //Login con usuario y contraseña
+    this.form = this.fb.group({
+      user: ['', Validators.required], //Para que el formulario de inicio de sesion sea valido hay que poner los datos bien
+      pass: ['', Validators.required]
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  done() { //funcion para validar usuario y contraseña
+    console.log(this.form);
+    const user = this.form.value.user; //muestra el usuario y el password escritos
+    const pass = this.form.value.pass;
+    
+    if (user == 'admin' && pass == '12345') { //condicional para iniciar sesion con admin
+      this.fakeLoading(); //hace la animacion de cargar y redirecciona
+    } else{
+      this.error(); //mensaje error si los datos son incorrectos
+      this.form.reset(); //resetea el formulario
+    }
+  }
+
+  error(){
+    this._snackBar.open('Incorrect User/Password', '', {
+      duration: 5000,
+      horizontalPosition:'center',
+      verticalPosition: 'bottom'
+    })
+  };
+
+  fakeLoading() {
+    this.loading = true;
+    setTimeout(() => {
+      this.loading = false;
+    }, 1500);
+  }
+  
+}
